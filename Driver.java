0f51b746834a072927cb72e29addb66b40446bba
Christina Y. Yu <cyyu@uvm.edu>
2013-12-06 15:20:08 -0500

    update driver

--- Driver.java
+++ Driver.java
@@ -2,14 +2,15 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
+/**
+   Driver class runs the WarGameFrame
+*/
 public class Driver
 {
 	public static void main(String[] args)
 	{
 		JFrame frame = new WarGameFrame();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		//frame.pack();
-      	// OR
       	frame.setSize(600, 450);
       	frame.validate();
       	frame.setVisible(true);
