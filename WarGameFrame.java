31abfed0d9191815881729511846e1d46b5d6353
Christina Y. Yu <cyyu@uvm.edu>
2013-12-06 16:49:01 -0500

    update WarGameFrame

--- WarGameFrame.java
+++ WarGameFrame.java
@@ -2,14 +2,21 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
+
+/**
+   WarGameFrame class is GUI for WarGame
+*/
 public class WarGameFrame extends JFrame
 {
 	WarGame game;
-	private JButton hitButton;
+	private JButton hitButton, restartButton;
 	private JPanel frame1, frame2, frame3, frame4, frame5, frame6, frame7, frame8;
-	private JLabel picFront1, picFront2, p1, p2, p1cards, p2cards, text;
+	private JLabel picFront1, picFront2, picBack1, picBack2, p1, p2, p1cards, p2cards, text;
 	private ImageIcon front1, front2, back;
 
+	/**
+	   Constructor creates a GridLayout Frame
+	*/
 	public WarGameFrame()
 	{
 		game = new WarGame();
@@ -23,33 +30,34 @@ public class WarGameFrame extends JFrame
 		p1cards = new JLabel("Cards in deck: " + game.getHand1().size());
 		frame1.add(p1, BorderLayout.NORTH);
 		frame1.add(p1cards, BorderLayout.CENTER);
-
 		back = new ImageIcon("back.gif");
-		JLabel picBack = new JLabel(back);
-		frame2 = new JPanel(new FlowLayout());
-		frame2.add(picBack);
+		picBack1 = new JLabel(back);
+		frame1.add(picBack1, BorderLayout.EAST);
+
+		frame2 = new JPanel(new BorderLayout());
+		picFront1 = new JLabel();
+		frame2.add(picFront1, BorderLayout.EAST);
 
 		frame3 = new JPanel();
-		picFront1 = new JLabel();
-		frame3.add(picFront1);
 
 		frame4 = new JPanel(new BorderLayout());
 		p2 = new JLabel("Player 2");
 		p2cards = new JLabel("Cards in deck: " + game.getHand2().size());
 		frame4.add(p2, BorderLayout.NORTH);
 		frame4.add(p2cards, BorderLayout.CENTER);
+		picBack2 = new JLabel(back);
+		frame4.add(picBack2, BorderLayout.EAST);
 
-		frame5 = new JPanel();
-		JLabel picBack2 = new JLabel(back);
-		frame5.add(picBack2);
+		frame5 = new JPanel(new BorderLayout());
+		picFront2 = new JLabel();
+		frame5.add(picFront2, BorderLayout.EAST);
 
 		frame6 = new JPanel();
-		picFront2 = new JLabel();
-		frame6.add(picFront2);
+
 
 		frame7 = new JPanel();
 		hitButton = new JButton("Next Move");
-		hitButton.addActionListener(new ButtonListener());
+		hitButton.addActionListener(new HitButtonListener());
 		frame7.add(hitButton);
 
 		frame8 = new JPanel();
@@ -66,7 +74,10 @@ public class WarGameFrame extends JFrame
 		add(frame8);
 	}
 
-	class ButtonListener implements ActionListener
+	/**
+	   HitButtonListener gives direction when the "next" button is pressed
+	*/
+	public class HitButtonListener implements ActionListener
 	{
 		public void actionPerformed(ActionEvent e)
 		{
@@ -81,8 +92,10 @@ public class WarGameFrame extends JFrame
 
 				if(game.getHand1().peek().getRank() > game.getHand2().peek().getRank())
 					num = 1;
-				else
+				else if(game.getHand1().peek().getRank() < game.getHand2().peek().getRank())
 					num = 2;
+				else
+					num = 0;
 
 				game.turn();
 
