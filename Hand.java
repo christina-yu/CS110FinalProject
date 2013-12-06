acfc5c7200ef07b5cf52ec305a0e11e3f70461cb
Christina Y. Yu <cyyu@uvm.edu>
2013-12-06 15:20:14 -0500

    update hand

--- Hand.java
+++ Hand.java
@@ -1,22 +1,16 @@
+/**
+   Hand class creates a hand object that is essentially a CardPile.
+   Use of a different class name with same functionality is due to
+   making it less confusing in the WarGame.
+*/
+
 public class Hand extends CardPile
 {
-	String name;
-
+	/**
+	   Default constructor creates a Hand
+	*/
 	public Hand()
 	{
 		super();
-	}
-
-	public Hand(String playerName)
-	{
-		super();
-
-		name = playerName;
-	}
-
-
-	public String toString()
-	{
-		return name;
 	}
 }
